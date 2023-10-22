32ff37e4a3f956e3088fc88fa52b01086e45706a
Joel Foster <j.foster@fetchrewards.com>
2023-10-22 17:16:24 -0500

    [MAIN] simple chat loop

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
+type chatCompletionResponse struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+
+	// an array of possible choices for the next message
+	Choices []struct {
+		Index   int `json:"index"`
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
+		FinishReason string `json:"finish_reason"`
+	} `json:"choices"`
+
+	// usage statistics
+	Usage struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+}
+
 func main() {
+	// Define API endpoint URL and authentication key
+	apiURL := "https://api.openai.com/v1/chat/completions"
+
+	// models
+	models := []string{
+		"gpt-4",
+		"gpt-3.5-turbo",
+	}
+
 	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
@@ -18,35 +54,68 @@ func main() {
 	}
 
 	// Get the API key from the environment variables
-	apiKey := os.Getenv("OPENAI_API_KEY")
-	if apiKey == "" {
+	authKey := os.Getenv("OPENAI_API_KEY")
+	if authKey == "" {
 		fmt.Println("Error: OPENAI_API_KEY environment variable not set")
 		return
 	}
 
-	// Set up the HTTP client with the API key
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.openai.com/v1/engines", nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
-	req.Header.Set("Authorization", "Bearer "+apiKey)
+	// Create function to send POST request to API
+	sendRequest := func(prompt string) (chatCompletionResponse, error) {
+		// Create request body
+		requestBody := strings.NewReader(fmt.Sprintf(`{"messages": [{"role": "user", "content": "%s"}], "model": "%s"}`, prompt, models[0]))
 
-	// Make the request to the OpenAI API
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return
-	}
-	defer resp.Body.Close()
+		// Create HTTP request
+		request, err := http.NewRequest("POST", apiURL, requestBody)
+		if err != nil {
+			return chatCompletionResponse{}, err
+		}
 
-	// Print the response status code and body
-	fmt.Println("Response status code:", resp.StatusCode)
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
+		// Set request headers
+		request.Header.Set("Content-Type", "application/json")
+		request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", authKey))
+
+		// Send request
+		client := &http.Client{}
+		response, err := client.Do(request)
+		if err != nil {
+			return chatCompletionResponse{}, err
+		}
+		defer response.Body.Close()
+
+		// Read response body
+		responseBody, err := io.ReadAll(response.Body)
+		if err != nil {
+			return chatCompletionResponse{}, err
+		}
+
+		// Parse response body
+		var r chatCompletionResponse
+		err = json.Unmarshal(responseBody, &r)
+		if err != nil {
+			return chatCompletionResponse{}, err
+		}
+
+		// Return the first choice
+		return r, nil
 	}
-	fmt.Println("Response body:", string(body))
+
+	// Create loop to continuously prompt user for input and send to API
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("You: ")
+		scanner.Scan()
+		userInput := scanner.Text()
+
+		response, err := sendRequest(userInput)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// for each choice in response, print the message
+		for _, choice := range response.Choices {
+			fmt.Printf("AI: %s\n", choice.Message.Content)
+		}
+	}
 }
